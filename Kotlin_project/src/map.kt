2fadd3c3193c127c3f2fe0b7f1334bf2fdbf08fd
LAPTOP-IJIAJ2UV\Prathmesh <prathmesh0690@gmail.com>
2025-02-13 11:02:15 +0530

    map example

---
+++ Kotlin_project/src/map.kt
@@ -0,0 +1,16 @@
+//import java.util.HashMap
+//
+//
+//fun main()
+//{
+//    var map= HashMapOf<Int, String>()
+//    map.put(2,"vishal")
+//    map.put(3,"wald")
+//    map.put(4,"cheese")
+//
+//    for(key in map.keys){
+//        println(map[key])
+//    }
+//
+//}
+//
